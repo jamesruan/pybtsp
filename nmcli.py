3cea17e3e9e1a03267304e9eb0dd79319bc009ca
James Ruan <ruanbeihong@gmail.com>
2017-09-06 16:39:11 +0800

    use list comprehension

--- nmcli.py
+++ nmcli.py
@@ -6,8 +6,10 @@ def parse(b):
     sa = re.split('\n', s)
     def splitcolumn(i):
         v = re.sub(r'\\:', r'-', i)
-        return re.split(r':', v)
-    return map(lambda i: splitcolumn(i), sa)
+        return tuple(re.split(r':', v))
+
+    r = [splitcolumn(x) for x in sa if len(x) > 0] 
+    return r
 
 def strip(b):
     s = jsonlpendec.from_bytes(b)
