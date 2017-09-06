b7eb01812c795fe4535a8c434164d0c9e29b7c3b
James Ruan <ruanbeihong@gmail.com>
2017-09-06 16:43:44 +0800

    add runSubProcess

--- wifi.py
+++ wifi.py
@@ -1,64 +1,76 @@
 import subprocess
 import nmcli
 
-def getWifiInterface():
+def runSubProc(args):
+    import os
+    po, pi = os.pipe()
     try:
-        fields = 'DEVICE,TYPE,STATE'
-        args = ['env', 'LC_ALL=C', 'nmcli', '-t', '-f', fields, 'device']
-        outf = subprocess.check_output(args, stderr = subprocess.STDOUT)
-        va =  nmcli.parse(outf)
+        out = subprocess.check_output(args, stderr = pi)
+        os.close(pi)
+        os.close(po)
+        return True, out
+    except subprocess.CalledProcessError as e:
+        out = os.read(po, 1<<12)
+        os.close(pi)
+        os.close(po)
+        return False, nmcli.strip(out)
+
+def getWifiInterface():
+    fields = 'DEVICE,TYPE,STATE'
+    args = ['env', 'LC_ALL=C', 'nmcli', '-t', '-f', fields, 'device']
+    ok, out = runSubProc(args)
+    if ok:
+        va =  nmcli.parse(out)
         r = {}
         for i in va:
             device, type, state = i
             if type == "wifi" and state != "unmanaged":
                 r[device] = state
         return r
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
-
+    else:
+        return {"error": out}
 
 def getScanResult():
-    try:
-        fields = 'SSID,MODE,CHAN,RATE,SIGNAL,SECURITY'
-        args = ['env', 'LC_ALL=C', 'nmcli', '-t', '-f', fields, 'device', 'wifi']
-        outf = subprocess.check_output(args, stderr = subprocess.STDOUT)
-        va =  nmcli.parse(outf)
+    fields = 'SSID,MODE,CHAN,RATE,SIGNAL,SECURITY'
+    args = ['env', 'LC_ALL=C', 'nmcli', '-t', '-f', fields, 'device', 'wifi']
+    ok, out = runSubProc(args)
+    if ok:
+        va = nmcli.parse(out)
         r = {}
         for i in va:
             ssid, mode, chan, rate, signal, enc = i
             r[ssid] = {"mode": mode, "channel": chan, "rate": rate, "signal": signal, "encryption": enc}
         return r
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
+    else:
+        return {"error": out}
 
 def getWifiConnection():
-    try:
-        fields = 'NAME,UUID,TYPE,DEVICE'
-        args = ['env', 'LC_ALL=C', 'nmcli', '-t', '-f', fields, 'connection']
-        outf = subprocess.check_output(args, stderr = subprocess.STDOUT)
-        va =  nmcli.parse(outf)
+    fields = 'NAME,UUID,TYPE,DEVICE'
+    args = ['env', 'LC_ALL=C', 'nmcli', '-t', '-f', fields, 'connection']
+    ok, out = runSubProc(args)
+    if ok:
+        va =  nmcli.parse(out)
         r = {}
         for i in va:
             name, uuid, type, device = i
             if type == "802-11-wireless":
                 r[uuid] = {"name": name, "device": device}
         return r
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
+    else:
+        return {"error": out}
 
 def activateWifiConnection(uuid, iface = None):
     if uuid == None:
         return {"error": "invalid parameter"}
-    try:
-        args = ['env', 'LC_ALL=C', 'nmcli', 'connection', 'up', uuid]
-        if iface:
-            args.append("ifname")
-            args.append(iface)
-        outf = subprocess.check_output(args, stderr= subprocess.STDOUT)
-        return {"msg": nmcli.strip(outf)}
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
-
+    args = ['env', 'LC_ALL=C', 'nmcli', 'connection', 'up', uuid]
+    if iface:
+        args.append("ifname")
+        args.append(iface)
+    ok, out = runSubProc(args)
+    if ok:
+        return {"msg": nmcli.strip(out)}
+    else:
+        return {"error": out}
 
 def createWifiConnection(ssid, password = None, iface=None):
     timeout = '20'
@@ -71,31 +83,31 @@ def createWifiConnection(ssid, password = None, iface=None):
     if iface:
         args.append("ifname")
         args.append(iface)
-    try:
-        outf = subprocess.check_output(args, stderr= subprocess.STDOUT)
-        return {"msg": nmcli.strip(outf)}
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
+    ok, out = runSubProc(args)
+    if ok:
+        return {"msg": nmcli.strip(out)}
+    else:
+        return {"error": out}
 
 def deleteWifiConnection(uuid):
     if uuid == None:
         return {"error": "invalid parameter"}
-    try:
-        args = ['env', 'LC_ALL=C', 'nmcli', 'connection', 'delete', uuid]
-        outf = subprocess.check_output(args, stderr= subprocess.STDOUT)
+    args = ['env', 'LC_ALL=C', 'nmcli', 'connection', 'delete', uuid]
+    ok, out = runSubProc(args)
+    if ok:
         return {"msg": "ok"}
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
+    else:
+        return {"error": out}
 
 def disconnectWifi(iface):
     if iface == None:
         return {"error": "invalid parameter"}
-    try:
-        args = ['env', 'LC_ALL=C', 'nmcli', 'device', 'disconnect', iface]
-        outf = subprocess.check_output(args, stderr= subprocess.STDOUT)
+    args = ['env', 'LC_ALL=C', 'nmcli', 'device', 'disconnect', iface]
+    ok, out = runSubProc(args)
+    if ok:
         return {"msg": "ok"}
-    except subprocess.CalledProcessError as e:
-        return {"error": nmcli.strip(e.output)}
+    else:
+        return {"error": out}
 
 if __name__ == "__main__":
     print(getWifiInterface())
